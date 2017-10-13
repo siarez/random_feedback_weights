48a8d34dd31cf940410dac460c813e723672fc0a
siavash <sia@siarez.com>
2017-10-13 15:44:43 -0700

    major refactoring, experiment setup, plotting, t-test, ...

--- random_feedback_weights.py
+++ random_feedback_weights.py
@@ -1,17 +1,18 @@
 from sklearn.datasets import fetch_mldata
 import torch
 import numpy as np
+from scipy.stats import ttest_ind
 import pickle
 import os
-import copy
+import math
 from matplotlib import pyplot as plt
-
+import csv
 
 dtype = torch.FloatTensor
 # dtype = torch.cuda.FloatTensor # Uncomment this to run on GPU
 # N is batch size; D_in is input dimension;
 # H is hidden dimension; D_out is output dimension.
-training_set_size, D_in, H, D_out = 40000, 28 * 28, 20, 10
+training_set_size, D_in, H, D_out = 50000, 28 * 28, 20, 10
 batch_size = 200
 testset_size = 2000
 randomWeights_path = "randomWeights"
@@ -20,32 +21,46 @@ randomWeights_path = "randomWeights"
 mnist = fetch_mldata('MNIST original', data_home='./')
 
 learning_rate = 0.07
-epochs = 50
+epochs = 200
 
-# create a list of random indices for training set
-train_idx = np.random.choice(len(mnist.data), training_set_size, replace=False)
-# create x and y by picking samples from the random indices
-mnist_x = np.array([mnist.data[i] for i in train_idx])
-mnist_x = torch.ByteTensor(mnist_x).type(dtype)
-mnist_y = np.array([[mnist.target[i] for i in train_idx]]).transpose()
-mnist_y = torch.DoubleTensor(mnist_y).type(torch.LongTensor)
 
-# One hot encoding
-y_onehot = torch.zeros([training_set_size, D_out]).type(dtype)
-y_onehot.scatter_(1, mnist_y, 1.0)
-mnist_x /= 255  # scaling down x to fall between 0 and 1
-x = torch.cat((mnist_x, torch.ones([training_set_size, 1])), 1)  # adding biases
+def create_dataset(dataset, set_size, one_hot_size, indices_to_exclude=None):
+    """
+    Takes in the MNIST dataset and picks a subset.
+    x is normalized to be in range 0 and 1
+    y (target) is one-hot encoded
+    :param dataset: MNIST dataset
+    :param set_size: number of samples in the dataset that is returned
+    :param one_hot_size: size of the one-hot target vectors
+    :param indices_to_exclude: When sampling from MNIST this indices are excluded
+    :return:
+    """
+    if (indices_to_exclude is not None):
+        _data = np.delete(dataset.data, indices_to_exclude, 0)
+        _target = np.delete(dataset.target, indices_to_exclude, 0)
+    else:
+        _data = dataset.data
+        _target = dataset.target
+    # create a list of random indices to pick the training set from
+    indices = np.random.choice(len(_data), set_size, replace=False)
+    # create x and y by picking samples from the random indices
+    mnist_x = np.array([_data[i] for i in indices])
+    mnist_x = torch.ByteTensor(mnist_x).type(dtype)
+    mnist_y = np.array([[_target[i] for i in indices]]).transpose()
+    mnist_y = torch.DoubleTensor(mnist_y).type(torch.LongTensor)
 
-x_batches = torch.split(x, batch_size)
-y_batches = torch.split(mnist_y, batch_size)
-y_onehot_batches = torch.split(y_onehot, batch_size)
-losses = [[], []]
+    # One hot encoding
+    _y_onehot = torch.zeros([set_size, one_hot_size]).type(dtype)
+    _y_onehot.scatter_(1, mnist_y, 1.0)
+    mnist_x /= 255  # scaling down x to fall between 0 and 1
+    _x = torch.cat((mnist_x, torch.ones([set_size, 1])), 1)  # adding biases
+    return _x, _y_onehot, indices
 
-# Setup plotting
-f, (ax1) = plt.subplots(1, 1, sharey=True)
-ax1.set_facecolor('black')
-f.set_dpi(200)
 
+def create_batches(x, y, _batch_size):
+    _x_batches = torch.split(x, _batch_size)
+    _y_batches = torch.split(y, _batch_size)
+    return _x_batches, _y_batches
 
 
 def sigmoid(logits):
@@ -63,103 +78,122 @@ def initialize_weights(fromFile=False):
     :param fromFile: If True, weights are initialized from file.
     :return: weight tensors
     """
-    if fromFile == True and os.path.isfile(randomWeights_path):
-        w1, w2, w2_feedback = pickle.load(open(randomWeights_path, "rb"))
-
+    if fromFile is True and os.path.isfile(randomWeights_path):
+        _w1, _w2, _w2_feedback = pickle.load(open(randomWeights_path, "rb"))
     else:
         # "+ 1"s are the bias terms
-        w1 = torch.randn(D_in + 1, H).type(dtype) / np.sqrt(D_in + 1)
-        w2 = torch.randn(H + 1, D_out).type(dtype) / np.sqrt(H + 1)
-        w2_feedback = torch.randn(H + 1, D_out).type(dtype) / np.sqrt(H + 1)
-        pickle.dump((w1, w2, w2_feedback), open(randomWeights_path, "wb"))
-    return w1, w2, w2_feedback
+        _w1 = torch.randn(D_in + 1, H).type(dtype) / np.sqrt(D_in + 1)
+        _w2 = torch.randn(H + 1, D_out).type(dtype) / np.sqrt(H + 1)
+        _w2_feedback = torch.randn(H + 1, D_out).type(dtype) / np.sqrt(H + 1)
+        pickle.dump((_w1, _w2, _w2_feedback), open(randomWeights_path, "wb"))
+    return _w1, _w2, _w2_feedback
 
 
-def calc_forward(input, w1, w2):
+def calc_forward(_input, _w1, _w2):
     """This function performs forward pass given `input`, `w1`, `w2`.
     It return class predictions and hidden layer values needed for back prop."""
-    hidden = sigmoid(input.mm(w1))
-    hidden_biased = torch.cat((hidden, torch.ones([batch_size, 1])), 1)  # adding biases
-    predictions = sigmoid(hidden_biased.mm(w2))
+    hidden = sigmoid(_input.mm(_w1))
+    hidden_biased = torch.cat((hidden, torch.ones([_input.shape[0], 1])), 1)  # adding biases
+    predictions = sigmoid(hidden_biased.mm(_w2))
     return predictions, hidden_biased
 
 
-def calc_backward(w2, hidden, prediction, target):
+def calc_backward(_w2, _input, hidden, prediction, target):
     """This function takes in w2, layer activations, and target.
      It returns gradients for weights"""
     delta_prediction = torch.mul((prediction - target), dSigmoid(prediction))
     gradient_w2 = hidden.t().mm(delta_prediction)
-    dError_dHidden = delta_prediction.mm(w2[:-1, :].t())
+    dError_dHidden = delta_prediction.mm(_w2[:-1, :].t())
     delta_hidden = torch.mul(dError_dHidden, dSigmoid(hidden[:, :-1]))
-    gradient_w1 = x_batches[b].t().mm(delta_hidden)
+    gradient_w1 = _input.t().mm(delta_hidden)
     # Update weights using gradient descent
     return gradient_w1, gradient_w2
 
-# Randomly initialize weights
-w1, w2, w2_random = initialize_weights(fromFile=True)
-w1_experiment = w1.clone()
-w2_experiment = w2.clone()
 
-for t in range(epochs):
-    num_of_batches = int(training_set_size / batch_size)
-    for b in range(num_of_batches):
-        # Forward pass: compute predicted y
-        y_pred, h_biased = calc_forward(x_batches[b], w1, w2)
-        if b == 0:
-            # Compute and print loss
-            loss = (y_pred - y_onehot_batches[b]).pow(2).sum()
-            losses[0].append(loss)
-            print(t, loss)
-        # Backprop to compute gradients of w1 and w2 with respect to loss
-        grad_w1, grad_w2 = calc_backward(w2, h_biased, y_pred, y_onehot_batches[b])
-        # Update weights using gradient descent
-        w1 -= learning_rate * grad_w1
-        w2 -= learning_rate * grad_w2
+accuracies = []  # holds accuracies for the two network for the test dataset for each experiment run
 
-        #doing forward and backward pass for the second network
-        # Forward pass: compute predicted y
-        y_pred, h_biased = calc_forward(x_batches[b], w1_experiment, w2_experiment)
-        if b == 0:
-            # Compute and print loss
-            loss = (y_pred - y_onehot_batches[b]).pow(2).sum()
-            losses[1].append(loss)
-            print(t, loss)
-        # Backprop to compute gradients of w1 and w2 with respect to loss
-        grad_w1, grad_w2 = calc_backward(w2_random, h_biased, y_pred, y_onehot_batches[b])
-        # Update weights using gradient descent
-        w1_experiment -= learning_rate * grad_w1
-        w2_experiment -= learning_rate * grad_w2
+for expr in range(20):
+    # Setup plotting
+    f, (ax1) = plt.subplots(1, 1, sharey=True)
+    ax1.set_facecolor('black')
+    f.set_dpi(200)
+    losses = [[], []]  # holds the losses for the two networks for each epoch in the experiment
+    x_training, y_training, indices_training = create_dataset(mnist, training_set_size, D_out)
+    x_training_batches, y_training_batches = create_batches(x_training, y_training, batch_size)
+    x_test, y_test, _ = create_dataset(mnist, testset_size, D_out, indices_training)
+    # Randomly initialize weights
+    w1, w2, w2_random = initialize_weights(fromFile=False)
+    # Experiment network (the one with random backprop w2) starts off with the same weights as the normal network
+    w1_experiment = w1.clone()
+    w2_experiment = w2.clone()
 
-normal_loss, = ax1.plot(losses[0], color='green', label='normal')
-random_loss, = ax1.plot(losses[1], color='orange', label='random w2')
-ax1.legend(handles=[normal_loss, random_loss])
-plt.show()
+    for t in range(epochs):
+        num_of_batches = int(training_set_size / batch_size)
+        for b in range(num_of_batches):
 
-# Starting test
-# removing samples that are present in training set
-data_training_removed = np.delete(mnist.data, train_idx, 0)
-target_training_removed = np.delete(mnist.target, train_idx, 0)
-# picking test data
-test_idx = np.random.choice(len(data_training_removed), testset_size)
-test_x = np.array([data_training_removed[i] for i in test_idx])
-test_x = torch.ByteTensor(test_x).type(dtype)
-test_y = np.array([[target_training_removed[i] for i in test_idx]]).transpose()
-test_y = torch.DoubleTensor(test_y).type(torch.LongTensor)
-test_x = torch.cat((test_x, torch.ones([testset_size, 1])), 1)  # adding biases
-# One hot encoding
-test_y_onehot = torch.zeros([testset_size, D_out]).type(dtype)
-test_y_onehot.scatter_(1, test_y, 1.0)
+            # doing forward and backward pass for the second network
+            # Forward pass: compute predicted y
+            y_pred_expr, h_biased_expr = calc_forward(x_training_batches[b], w1_experiment, w2_experiment)
+            if b == 0:
+                # Compute and print loss
+                loss = (y_pred_expr - y_training_batches[b]).pow(2).sum()
+                losses[1].append(loss)
+                print(t, loss)
+            # Backprop to compute gradients of w1 and w2 with respect to loss
+            grad_w1_expr, grad_w2_expr = calc_backward(w2_random * math.log(t+1), x_training_batches[b], h_biased_expr, y_pred_expr, y_training_batches[b])
+            # Update weights using gradient descent
+            w1_experiment -= learning_rate * grad_w1_expr
+            w2_experiment -= learning_rate * grad_w2_expr
 
-# Forward pass: compute predicted y
-h_logits = test_x.mm(w1)
-h = sigmoid(h_logits)
-h_biased = torch.cat((h, torch.ones([testset_size, 1])), 1)  # adding biases
-y_logits = h_biased.mm(w2)
-y_pred = sigmoid(y_logits)
+            # Forward pass: compute predicted y
+            y_pred, h_biased = calc_forward(x_training_batches[b], w1, w2)
+            if b == 0:
+                # Compute and print loss
+                loss = (y_pred - y_training_batches[b]).pow(2).sum()
+                losses[0].append(loss)
+                print(t, loss)
+            # Backprop to compute gradients of w1 and w2 with respect to loss
+            grad_w1, grad_w2 = calc_backward(w2, x_training_batches[b], h_biased, y_pred, y_training_batches[b])
+            # Update weights using gradient descent
+            w1 -= learning_rate * grad_w1
+            w2 -= learning_rate * grad_w2
 
-# Compute and print loss for testset
-_, predicted_classes = torch.max(y_pred, dim=1)
-accuracy = torch.sum(torch.eq(predicted_classes, test_y[:, 0])) / testset_size
-print('test accuracy: ', accuracy)
+        # w2_random *= 1.02
 
 
+    normal_loss, = ax1.plot(losses[0], color='green', label='normal',  linewidth=1)
+    random_loss, = ax1.plot(losses[1], color='orange', label='random w2',  linewidth=1)
+    ax1.legend(handles=[normal_loss, random_loss])
+    ax1.set_ylim([0, 250])
+    plt.savefig("expr-" + str(expr+1) + ".png")
+
+    # Starting test
+    # Forward pass: compute predicted y
+    y_pred, _ = calc_forward(x_test, w1, w2)
+    y_pred_experiment, _ = calc_forward(x_test, w1_experiment, w2_experiment)
+
+
+    # Compute and print loss for testset
+    _, predicted_classes = torch.max(y_pred, dim=1)
+    _, predicted_classes_experiment = torch.max(y_pred_experiment, dim=1)
+    _, target_class = torch.max(y_test, dim=1)
+    accuracy = torch.sum(torch.eq(predicted_classes, target_class)) / testset_size
+    accuracy_experiment = torch.sum(torch.eq(predicted_classes_experiment, target_class)) / testset_size
+    print('test accuracies: ', accuracy, accuracy_experiment)
+    accuracies.append((accuracy, accuracy_experiment))
+
+# calculating t-statistics to judge if the difference is significant
+accuracies_np = np.array(accuracies)
+t_statistic, p_val = ttest_ind(accuracies_np[:, 0], accuracies_np[:, 1], equal_var=False)
+print("t stat and p val", t_statistic, p_val)
+
+with open('accuracies.csv', 'w') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    csvwriter.writerow(["normal", "random w2"])
+    for row in accuracies:
+        csvwriter.writerow([row[0], row[1]])
+    csvwriter.writerow(["averages"])
+    csvwriter.writerow([accuracies_np[:, 0].mean(), accuracies_np[:, 1].mean()])
+    csvwriter.writerow(["t-statistic", "p value"])
+    csvwriter.writerow([t_statistic, p_val])
